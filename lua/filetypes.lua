a2d8d429363b1a9454358069d129629989394622
Strongleong <nikita_chul@mail.ru>
2023-04-12 05:37:54 +1100

    Added autocmd for setting QML filetype

--- lua/filetypes.lua
+++ lua/filetypes.lua
@@ -46,6 +46,12 @@ vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {
   desc    = 'Rasi file type (rofi config)'
 })
 
+vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {
+  pattern = {'*.qml'},
+  command = 'set ft=qml',
+  desc    = 'QML file type (SDDM theme scripting)'
+})
+
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'python',
   command = 'setl softtabstop=4 shiftwidth=4 tabstop=4 textwidth=100',
