a7ac5776e491c54741c4b9b34d31c47f1ca62bbb
strongleong <nikita_chul@mail.ru>
2023-11-25 11:06:47 +1100

    Changed live_grep to live_grep_args in alpha

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -42,7 +42,7 @@ return {
 			dashboard.button("f", "󰈞  Find file",           ":Telescope find_files <CR>"               ),
 			dashboard.button("e", "󰈔  New file",            ":ene <BAR> startinsert <CR>"              ),
 			dashboard.button("r", "󱇧  Recently used files", ":Telescope oldfiles <CR>"                 ),
-			dashboard.button("a", "󰊄  Find text",           ":Telescope live_grep <CR>"                ),
+			dashboard.button("a", "󰊄  Find text",           ":Telescope live_grep_args <CR>"           ),
 			dashboard.button("c", "  Configuration",       ":cd ~/.config/nvim/ <BAR> e init.lua<CR>" ),
 			dashboard.button("n", "  Random title",        ":AlphaRandomTitle<CR>"                    ),
 			dashboard.button("t", "  Telescope title",     ":AlphaTelescopeTitle<CR>"                 ),
