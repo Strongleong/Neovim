3f7c9c103b93a66bcdb2f3ea997699ae0921292c
strongleong <nikita_chul@mail.ru>
2023-11-25 11:07:11 +1100

    Fixed dap config

--- lua/plugins/2_dap.lua
+++ lua/plugins/2_dap.lua
@@ -1,8 +1,44 @@
 return {
-  'mfussenegger/nvim-dap',
-  'rcarriga/nvim-dap-ui',
-  'rcarriga/cmp-dap',
-  'nvim-telescope/telescope-dap.nvim',
-  'theHamsta/nvim-dap-virtual-text',
-  'jay-babu/mason-nvim-dap.nvim',
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = {
+      'mfussenegger/nvim-dap',
+    }
+  },
+
+  {
+    'rcarriga/cmp-dap',
+    dependencies = {
+      'mfussenegger/nvim-dap',
+    }
+  },
+  {
+    'nvim-telescope/telescope-dap.nvim',
+    dependencies = {
+      'mfussenegger/nvim-dap',
+    }
+  },
+  {
+    'theHamsta/nvim-dap-virtual-text',
+    dependencies = {
+      'mfussenegger/nvim-dap',
+    }
+  },
+  {
+    'jay-babu/mason-nvim-dap.nvim',
+    dependencies = {
+      'mfussenegger/nvim-dap',
+    }
+  },
+
+  {
+    'mfussenegger/nvim-dap',
+    config = function()
+      local dap = require('dap')
+      dap.listeners.before['event_breakpoint']['my-plugin'] = function(session, body)
+        print('Session terminated', vim.inspect(session), vim.inspect(body))
+      end
+    end
+  }
+
 }
