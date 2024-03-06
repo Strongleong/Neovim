63b9260e6770f7770b2e02c54ed66e7aafa2cf7b
Никита Чулков <nikita_chul@mail.ru>
2024-03-06 16:47:46 +1100

    Updated php snippets

--- lua/snippets/php.lua
+++ lua/snippets/php.lua
@@ -19,15 +19,83 @@ return {
       }
       ]],
       {
-        i(1, "Type"),
-        i(2, "var"),
-        rep(2),
-        rep(1),
-        rep(1),
-        rep(2),
-        rep(2),
-        rep(1),
-        rep(2),
+        i(1, "Type"),   -- @var Type
+        i(2, "var"),    --              name
+        rep(2),         -- private Type
+        rep(1),         -- @return Type
+        rep(1),         -- getType()
+        rep(2),         -- if $this->name
+        rep(2),         -- $this->name =
+        rep(1),         --               new Type
+        rep(2),         -- return $this->name
+      }
+    )
+  ),
+  s(
+    "lzz",
+    fmta(
+      [[
+      /** @var <> $<> */
+      private <> $<>;
+
+      /**
+       * @return <>
+       */
+      protected function get<>(): <>
+      {
+          if ($this->><> === null) {
+              $this->><> = new <>();
+          }
+
+          return $this->><>;
+      }
+      ]],
+      {
+        i(1, "Type"), -- @var Type
+        i(2, "var"),  --              name
+        rep(1),       -- private Type
+        rep(2),       --              name
+        rep(1),       -- @return Type
+        rep(1),       -- getType()
+        rep(1),       --          : Type
+        rep(2),       -- if $this->name
+        rep(2),       -- $this->name =
+        rep(1),       --               new Type
+        rep(2),       -- return $this->name
+      }
+    )
+  ),
+  s(
+    "lzn",
+    fmta(
+      [[
+      /** @var <> $<> */
+      private ?<> $<> = null;
+
+      /**
+       * @return <>
+       */
+      protected function get<>(): <>
+      {
+          if ($this->><> === null) {
+              $this->><> = new <>();
+          }
+
+          return $this->><>;
+      }
+      ]],
+      {
+        i(1, "Type"), -- @var ?Type
+        i(2, "var"),  --              name
+        rep(1),       -- private ?Type
+        rep(2),       --              name
+        rep(1),       -- @return Type
+        rep(1),       -- getType()
+        rep(1),       --          : Type
+        rep(2),       -- if $this->name
+        rep(2),       -- $this->name =
+        rep(1),       --               new Type
+        rep(2),       -- return $this->name
       }
     )
   ),
